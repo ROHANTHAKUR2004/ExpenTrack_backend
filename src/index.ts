483d594fd04ca27f13eccef341eb6112c4fe2f8d
rohan <rohanthakur89768@gmail.com>
2025-02-07 18:11:11 +0530

    fixed the url bug

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://expen-track-backend.vercel.app",
+      "https://expen-track-frontend.vercel.app",
       "http://localhost:5173",
     ],
     credentials: true,
