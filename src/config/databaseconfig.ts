2401c064e10750a886ec1b740345f546636597e3
rohan <rohanthakur89768@gmail.com>
2025-02-10 16:12:58 +0530

    congif the bug

--- src/config/databaseconfig.ts
+++ src/config/databaseconfig.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-//const uri: string = "mongodb://127.0.0.1:27017/expense-manager";
+const uri: string = "mongodb+srv://rohanthakur89768:dvLTfVxMiUI43sgT@cluster0.cxmlf.mongodb.net/";
 
 async function connectDatabase() {
   try {
     const response = await mongoose.connect("mongodb+srv://rohanthakur89768:dvLTfVxMiUI43sgT@cluster0.cxmlf.mongodb.net/");
-    console.log(`database is connected with :${response.connection}`);
+    console.log(`database is connected with :${response.connection.host}`);
   } catch (error) {
     console.log(`failed to connect with database !`);
     process.exit(1);
